089cf0fddc2a65e49259171092e286af3c50600b
Rasan98 <rasanmon98@gmail.com>
2020-02-19 16:02:19 +0100

    modified Practica1

--- Practica1.py
+++ Practica1.py
@@ -5,12 +5,27 @@ def carga_csv(file_name):
     valores = read_csv(file_name, header = None).values
     return valores.astype(float)
 
-def hipotesis(th0, th1, x)
-    return th0 + th1*x
+def hipotesis(X, Theta):
+    return np.dot(X,Theta)
 
-def coste()
+def coste(X, Y, Theta):
+    H = np.dot(X, Theta)
+    aux = (H - Y) ** 2
+    return aux.sum() / (2*len(X))
 
-
+def descenso_gradiente(X, Y, alpha):
+    theta= np.array([[0], [0]])
+    for i in np.arange(6):
+        aux = hipotesis(X, theta)
+        temp0 = theta[0] - alpha*(1/len(X))*np.sum(aux - Y)
+        aux2 = aux - Y
+        aux2 = np.dot(aux2, X)
+        temp1 = theta[1] - alpha*(1/len(X))*np.sum(aux2)
+        theta[0] = temp0
+        theta[1] = temp1
+        print(theta)
+        print(coste(X,Y, theta))
+         
 
 datos = carga_csv('ex1data1.csv')
 
@@ -22,8 +37,9 @@ n = np.shape(X)[1]
 
 X = np.hstack([np.ones([m,1]), X])
 
-print(X)
-
 alpha = 0.01
 
-thetas, costes = descenso_gradiente(X, Y, alpha) 
+#thetas, costes = 
+descenso_gradiente(X, Y, alpha) 
+
+print("FIN")
