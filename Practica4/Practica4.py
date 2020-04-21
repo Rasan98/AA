772659fce9a331af4733f1f811d0b17585c2f47b
Rasan98 <rasanmon98@gmail.com>
2020-04-21 12:27:52 +0300

    Finished debugging gradient. Now starting precision calculation.

--- Practica4/Practica4.py
+++ Practica4/Practica4.py
@@ -2,11 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat     
 import checkNNGradients as chk
+import scipy.optimize as opt
+
 def sigmoide(X):
     return 1/(1+np.exp(-X))
-
-def sigmoide_derivada(X):
-    return sigmoide(X)*(1-sigmoide(X))
 
 def pesosAleatorios(L_in, L_out):
     eini = np.sqrt(6)/np.sqrt(L_in + L_out)
@@ -23,9 +22,9 @@ def forwprop(theta1, theta2, X):
     return a2.transpose(), a3.transpose()
 
 def coste(theta1, theta2, X, y, lda, H):
-    aux = (-y*np.log((H + 1e-6))) - ((1-y)*np.log((1-H + 1e-6)))
+    aux = (-y*np.log((H))) - ((1-y)*np.log((1-H)))
     aux = (1 / (len(X))) * np.sum(aux)
-    aux2 = np.sum(theta1 ** 2) + np.sum(theta2 ** 2)
+    aux2 = np.sum(theta1[:,1:] ** 2) + np.sum(theta2[:,1:] ** 2)
     aux2 = (aux2*lda)/(2*len(X))
     return aux + aux2
 
@@ -36,25 +35,27 @@ def backprop(params_rn, num_entradas, num_ocultas, num_etiquetas, X, y, reg):
     theta2 = np.reshape(params_rn[num_ocultas * (num_entradas + 1):], (num_etiquetas, (num_ocultas + 1)))
     m = X.shape[0]       
     a1 = np.hstack([np.ones([m, 1]), X])
+    
     a2, h = forwprop(theta1, theta2, a1)       
     cost = coste(theta1, theta2, a1, y, reg, h)       
-    delta3 = np.zeros(num_entradas) 
-    delta2 = np.zeros(num_ocultas+1)
+    
     inc1 = np.zeros((theta1.shape[0], theta1.shape[1]))
     inc2 = np.zeros((theta2.shape[0], theta2.shape[1]))   
-    for i in range(len(X)):
-        delta3 = h[i,:] - y[i,:] 
+    for i in range(m):
+        delta3 = h[i] - y[i] 
         delta3 = delta3[:,np.newaxis]
         delta2 = np.dot(theta2.transpose(), delta3) * ((a2[i] * (1-a2[i]))[:,np.newaxis])
-        delta2 = np.delete(delta2, (0))[:,np.newaxis]
+        
         aux = a1[i]
-        inc1 = inc1 + np.dot(delta2, aux[:,np.newaxis].transpose())
+        inc1 = inc1 + np.dot(delta2[1:], aux[np.newaxis,:])
         aux = a2[i]
-        inc2 = inc2 + np.dot(delta3, aux[:,np.newaxis].transpose())
-    D1 = (1/m)*inc1
-    #D1[:,1:] = D1[:,1:] + reg*theta1[:,1:]
-    D2 = (1/m)*inc2
-    #D2[:,1:] = D2[:,1:] + reg*theta2[:,1:]
+        inc2 = inc2 + np.dot(delta3, aux[np.newaxis,:])
+
+    D1 = inc1/m
+    D1[:,1:] = D1[:,1:] + (reg/m)*theta1[:,1:]
+    D2 = inc2/m
+    D2[:,1:] = D2[:,1:] + (reg/m)*theta2[:,1:]
+    #print(cost)
     return cost, np.concatenate((np.ravel(D1), np.ravel(D2)))
 
 def codificaY(Y, num_etiquetas):
@@ -79,7 +80,10 @@ params_2 = pesosAleatorios(num_ocultas, num_etiquetas)
 params_rn = np.concatenate((np.ravel(params_1), np.ravel(params_2)))
 reg = 1
 
-print(chk.checkNNGradients(backprop, 0))
+#print(chk.checkNNGradients(backprop, 1))
 #print(backprop(params_rn, num_entradas, num_ocultas, num_etiquetas,X, Y, reg))
+
+opt.minimize(fun=backprop, x0=params_rn, args=(num_entradas, num_ocultas, num_etiquetas, X, Y, reg),
+              method="TNC", jac = True, options={"maxite = 70"})
 
 print("Fin" * 5)
