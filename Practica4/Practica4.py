ffaedb076f7ab603e11e220ee3ee6849e1d90080
Rasan98 <rasanmon98@gmail.com>
2020-05-03 12:42:16 +0300

    Finished part1 of Practica5

--- Practica4/Practica4.py
+++ Practica4/Practica4.py
@@ -55,6 +55,28 @@ def backprop(params_rn, num_entradas, num_ocultas, num_etiquetas, X, y, reg):
     #print(cost)
     return cost, np.concatenate((np.ravel(D1), np.ravel(D2)))
 
+def backprop_rec(params_rn, num_entradas, num_ocultas, num_etiquetas, X, y, reg):
+    theta1 = np.reshape(params_rn[: (num_ocultas * (num_entradas + 1))], (num_ocultas, (num_entradas+1)))
+    theta2 = np.reshape(params_rn[num_ocultas * (num_entradas + 1):], (num_etiquetas, (num_ocultas + 1)))
+    
+    m = X.shape[0]       
+    a1 = np.hstack([np.ones([m, 1]), X])
+    
+    a2, h = forwprop(theta1, theta2, a1)       
+    cost = coste(theta1, theta2, m, y, reg, h)       
+    
+    delta3 = h - y 
+    delta2 = np.dot(theta2.transpose(), delta3.transpose()).transpose() * (a2 * (1-a2))
+    delta2 = delta2[:,1:]
+    inc1 = np.dot(delta2.transpose(), a1)
+    inc2 = np.dot(delta3.transpose(), a2)
+    D1 = inc1/m
+    D1[:,1:] = D1[:,1:] + (reg/m)*theta1[:,1:]
+    D2 = inc2/m
+    D2[:,1:] = D2[:,1:] + (reg/m)*theta2[:,1:]
+    #print(cost)
+    return cost, np.concatenate((np.ravel(D1), np.ravel(D2)))
+
 def codificaY(Y, num_etiquetas):
     Yp = np.zeros((Y.shape[0], num_etiquetas + 1))
     Yp[[np.arange(Y.shape[0])], Y[:,0]] = 1
@@ -91,7 +113,7 @@ reg = 1
 print(chk.checkNNGradients(backprop, 1))
 #print(backprop(params_rn, num_entradas, num_ocultas, num_etiquetas,X, Y, reg))
 
-res = opt.minimize(fun=backprop, x0=params_rn, args=(num_entradas, num_ocultas, num_etiquetas, X, y, reg),
+res = opt.minimize(fun=backprop_rec, x0=params_rn, args=(num_entradas, num_ocultas, num_etiquetas, X, y, reg),
                     method="TNC", jac = True, options={"maxiter":70})
 
 thetas = res.x
---
+++ Practica5/Practica5_p2.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat     
+import scipy.optimize as opt
+
+def hipotesis(X, Theta):
+    return np.dot(X,Theta)
+    
+
+def costeYgrad(Theta, X, Y, reg):
+    m = X.shape[0]
+    x = np.hstack([np.ones([m, 1]), X])
+    H = hipotesis(x, Theta)
+    coste = (np.sum((H-Y.ravel()) ** 2))/(2*m) + ((Theta[1] ** 2) * reg)/(2*m)
+    grad = np.sum((H-Y.ravel())[:,np.newaxis]*x,0)/m
+    grad[1:] += (reg/m)*(Theta[1:] ** 2)
+    return coste, grad
+
+data = loadmat("ex5data1.mat")
+X = data['X']
+Y = data['y']
+m = X.shape[0]
+Xval = data['Xval']
+Yval = data['yval']
+
+Xtest = data['Xtest']
+Ytest = data['ytest']
+
+Theta = np.array([1,1])
+
+reg = 0
+
+print(costeYgrad(Theta, X, Y, 1))
+
+res = opt.minimize(fun=costeYgrad, x0=Theta, args=(X, Y, reg),
+                    method="TNC", jac = True, options={"maxiter":70})
+
+plt.figure()
+plt.scatter(X, Y, c="red", marker='x')
+plt.plot(X, hipotesis(np.hstack([np.ones([m, 1]), X]),res.x), c="blue", linestyle='-')
+plt.legend()
+plt.xlabel("Change in water level (x)")
+plt.ylabel("Wate flowing out of the dam (y)")
+plt.savefig("line.png")
+
+print("Fin"*5)
